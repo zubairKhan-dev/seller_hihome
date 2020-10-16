977c058cc7629836fe6bc5be84248b34fef87fa5
Sohrab Hussain <sohrab.iosdev@gmail.com>
2020-10-16 15:42:08 +0400

    AppIcon Added for Android App.

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -3,7 +3,8 @@ export const SMS_API_Key = "2f91f01e";
 export const SMS_API_Secret = "mea6QqIDYTCLb7kI";
 
 // DEVELOPMENT
-// export const CODE_PUSH_KEY = "NmbqpVKflleqvd3AGiGAyvImLeT25tDPJhIsH";
+// export const CODE_PUSH_KEY_ANDROID = "IpAg2HEuKnsAOQRe4TeCrKju3biB1afxzA4Me";
+// export const CODE_PUSH_KEY_IOS = "NmbqpVKflleqvd3AGiGAyvImLeT25tDPJhIsH";
 // export const ONESIGNAL_APP_ID = "35f7105d-d2a5-4b44-8f1f-28ea14dfd065";
 // export const DISPLAY_NAME = "SellerDev"
 // export const BUNDLE_IDENTIFIER = "com.aziz.seller"
@@ -11,7 +12,8 @@ export const SMS_API_Secret = "mea6QqIDYTCLb7kI";
 // export const APP_VERSION = "1.0.1"
 
 // PRODUCTION
-export const CODE_PUSH_KEY = "z2JSc-SS-Nh0aVcVX_pXC6Y1gdqq3Vqda3Xs5";
+export const CODE_PUSH_KEY_ANDROID = "Cvfx-gQZBKqofEHkfH7C1HXfPX1zRQHKrgYTY";
+export const CODE_PUSH_KEY_IOS = "z2JSc-SS-Nh0aVcVX_pXC6Y1gdqq3Vqda3Xs5";
 export const ONESIGNAL_APP_ID = "762be659-3ba5-4e89-83e9-48d9c8586c12";
 export const DISPLAY_NAME = "Seller"
 export const BUNDLE_IDENTIFIER = "com.hihome.seller"
